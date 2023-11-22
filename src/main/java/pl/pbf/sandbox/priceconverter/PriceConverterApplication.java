b65afb4929f834059747df982b44047fbf9e2b43
bachta.pawel <bachta.pawel@gmail.com>
2023-11-22 20:50:40 +0100

    init

---
+++ src/main/java/pl/pbf/sandbox/priceconverter/PriceConverterApplication.java
@@ -0,0 +1,12 @@
+package pl.pbf.sandbox.priceconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceConverterApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/pbf/sandbox/priceconverter/PriceConverterApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.pbf.sandbox.priceconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
