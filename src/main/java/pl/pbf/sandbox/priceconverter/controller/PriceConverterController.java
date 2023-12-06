5366b2a7c04ca05a56ebca00d69146de0f3a46e7
bachta.pawel <bachta.pawel@gmail.com>
2023-12-06 21:08:45 +0100

    add swagger-ui

--- src/main/java/pl/pbf/sandbox/priceconverter/controller/PriceConverterController.java
+++ src/main/java/pl/pbf/sandbox/priceconverter/controller/PriceConverterController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import pl.pbf.sandbox.priceconverter.controller.model.PriceConverterResponse;
 import pl.pbf.sandbox.priceconverter.service.PriceConverterService;
@@ -14,10 +16,12 @@ import pl.pbf.sandbox.priceconverter.service.PriceConverterService;
 @RestController
 @RequestMapping("/convert")
 @RequiredArgsConstructor
+@Tag(name = "Price Converter")
 public class PriceConverterController {
 
     private final PriceConverterService priceConverterService;
 
+    @Operation(summary = "Convert provided price with currency to PLN price")
     @GetMapping("/{currency}/{price}")
     public PriceConverterResponse convertPriceToPln(@PathVariable("currency") final String currency,
             @PathVariable("price") final BigDecimal price) {
